8833e147b08592de025abd57eb6ea6c396a49bb4
Nunzia Biele <nunzia.biele@virgilio.it>
2024-06-04 15:47:10 +0200

    esercizi su DOM

---
+++ manipolazionedom1.js
@@ -0,0 +1,13 @@
+// Crea la tua To-Do List mediante i metodi visti nel video.
+// Crea una funzione che ti permette di:
+
+// - Creare una lista aggiungendo il task desiderato mediante il bottone `Aggiungi`.
+// - Creare un checkbox per ogni task aggiunto.
+const input = document.querySelector("ul");
+const addProduct = () => {
+  const list = document.createElement("li");
+  list.innerText = "studiare";
+  ul.appendChild(list);
+  const createCheckbox = document.createElement("checkbox")
+createCheckbox.innerText = list
+};
